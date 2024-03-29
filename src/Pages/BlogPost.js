42c3abd0a0e6ec7822a179823a86f287c0c684a9
Maeva <maeva-froger@outlook.fr>
2024-03-29 16:22:17 +0100

    start editing

--- src/Pages/BlogPost.js
+++ src/Pages/BlogPost.js
@@ -1,25 +1,52 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import FetchPostId from "../components/FetchPostId";
+import EditBlogPost from "../components/EditBlogPost";
 
 const BlogPost = () => {
     const { id } = useParams();
     const [post, setPost] = useState({});
+
     useEffect(() => {
         FetchPostId(id, "GET").then((post) => {
             setPost(post);
         });
     }, [id]);
 
+const handleChange = (name, value) => {
+    setPost((prevState) => ({
+        ...prevState,
+        [name]: value,
+    }));
+    console.log(name, value)
+};
+
+    function save() {
+        FetchPostId(id, "PUT").then((updatedPost) => {
+            setPost(updatedPost);
+            console.log({post})
+        });
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        save()
+        alert(`Votre post a bien été modifié !`);
+    };
     return (
         <>
-        {/* Créer un composant post avec titre etc + style */}
+            {/* Créer un composant post avec titre etc + style */}
             <h1>Post {id}</h1>
             <h2>{post.title}</h2>
             <p>{post.body}</p>
-            <Link to={`/post/${post.id}/edit`}>
-                <button>Edit</button>
-            </Link>
+            <button>Edit</button>
+            <EditBlogPost
+                onSubmit={handleSubmit}
+                // Revoir onEdit
+                onEditTitle={(value) => handleChange("title", value)}
+                onEditBody={(value) => handleChange("body", value)}
+                titleValue={post.title}
+                bodyValue={post.body}
+            />
         </>
     );
 };
--- src/components/EditBlogPost.js
+++ src/components/EditBlogPost.js
@@ -1,14 +1,29 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+// import { useEffect, useState } from "react";
+// import FetchPostId from "../components/FetchPostId";
+// import { useParams } from "react-router";
 
-const EditBlogPost = () => {
-    const [title, setTitle] = useState("");
-    const [body, setBody] = useState("");
-    const { id } = useParams();
-
+const EditBlogPost = ({onSubmit,onEditTitle, onEditBody, titleValue, bodyValue}) => {
     return (
         <>
-            <h1>Test edit blog post</h1>
+            <h1>COMPOSANT EDIT</h1>
+            {/* Créer composant form */}
+            <form onSubmit={onSubmit}>
+                <input
+                    placeholder={titleValue}
+                    // Revoir onChange
+                    onChange={(e) => onEditTitle(e.target.value)}
+                    name={titleValue}
+                    type="text"
+                />
+                <textarea
+                    placeholder={bodyValue}
+                    // Revoir onChange
+                    onChange={(e) => onEditBody(e.target.value)}
+                    name={bodyValue}
+                    type="text"
+                />
+                <button type="submit">Terminer l'édition</button>
+            </form>
         </>
     );
 };
