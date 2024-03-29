8592e54d4f52b37410589d3580fd6a69226c3c1a
Stella Ndjikessi <stelladiana@live.fr>
2024-03-29 11:21:35 +0100

    LocalStorage of posts

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,5 +1,19 @@
-const BlogPost = () => {
-  return (<h1>Test blog post</h1>)
+const BlogPost = ({testsPosts}) => {
+  return (
+    <>
+      <h1>Test</h1>
+      {/* {testsPosts.map((post, index) => {
+        return (
+          <div key={index}>
+            <p>{post.id}</p>
+            <p>{post.title}</p>
+            <p>{post.body}</p>
+          </div>
+        )
+      })}
+      {console.log(testsPosts)} */}
+    </>
+  )
 }
 
 export default BlogPost;
--- src/routing/HomePage.js
+++ src/routing/HomePage.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
+import BlogPost from '../components/BlogPost';
 
 const url = 'https://jsonplaceholder.typicode.com/posts';
 
 function HomePage() {
-  const [posts, setPosts] = useState("");
+  const [posts, setPosts] = useState();
+  console.log(posts);
 
   useEffect(() => {
     // @TODO - fetching post blogs from the server...
@@ -17,6 +19,13 @@ function HomePage() {
       })
       .then(postsData => {
         setPosts(postsData);
+        let postInfos = JSON.parse(localStorage.getItem('PostInfosTest'));
+        if(!postInfos) {
+          postInfos = posts;
+        } else {
+          postInfos.push(posts);
+        }
+        localStorage.setItem('PostInfosTest', JSON.stringify(postInfos));
       })
       .catch(error => {
         console.log('Error :', error)
@@ -26,12 +35,29 @@ function HomePage() {
     // @HINT - once we got the data, it's maybe time to provoke a rerender...
   }, []);
   // pay attention on how to use carefully useEffect, don't hesitate to log !
-  console.log(posts);
-  
+
+  const testPosts = [
+    {
+      id: 1,
+      title: "Test",
+      body: "Bla bla bla",
+    },
+    {
+      id: 2,
+      title: "Test2",
+      body: "Bla2 bla bla",
+    },
+    {
+      id: 2,
+      title: "Test3",
+      body: "Bla3 bla bla",
+    },
+  ]
+
   return (
     <>
       <h1>Blog posts</h1>
-      <p>...</p>
+      <BlogPost posts={testPosts}/>
     </>
   );
 }
